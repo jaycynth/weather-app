22c237222c2cd380f99c857134474de6c4bfb179
Juliana <juliekivuva@gmail.com>
2024-06-12 09:51:00 +0300

    project setup

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:6.19.0")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
+    id("com.diffplug.spotless") version "6.19.0" apply  false
+
+}
+
+subprojects {
+    afterEvaluate {
+        project.apply("../spotless.gradle")
+    }
+}
